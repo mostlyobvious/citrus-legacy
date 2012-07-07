b406d18fca554ad8b43f6ed88faabcf4d5ce73b0
Paweł Pacana <pawel.pacana@gmail.com>
2012-07-08 01:06:37 +0200

    Change matched string since test should fail bu did not.

--- test/acceptance/user_adds_project_to_continous_integration_test.rb
+++ test/acceptance/user_adds_project_to_continous_integration_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserAddsProjectToContinousIntegrationTest < AcceptanceTestCase
   def test_user_can_add_github_project
     repo  = 'git://github.com/pawelpacana/citrus.git'
-    name  = 'citrus'
+    name  = 'awesome project'
 
     @user.add_project_from_cmdline(name, repo)
     @user.visit_projects
