78271750cb643ecf4a267b5ce7d0435560ea1bbe
Paweł Pacana <pawel.pacana@gmail.com>
2012-07-08 14:22:43 +0200

    Include validations from active_model.

--- lib/citrus/entity.rb
+++ lib/citrus/entity.rb
@@ -2,19 +2,7 @@ require 'citrus'
 
 module Citrus
   class Entity
+    include ActiveModel::Validations
     include Virtus
-
-    def valid?
-      return true unless self.class.required_attributes
-      self.class.required_attributes.all? { |attr| !!send(attr) }
-    end
-
-    class << self
-      attr_accessor :required_attributes
-
-      def validates_presence_of(*attrs)
-        self.required_attributes = attrs
-      end
-    end
   end
 end
