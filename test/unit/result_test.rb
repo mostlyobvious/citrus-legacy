7e18b971e546ba37ab7859efb7cbe870cda6af10
Paweł Pacana <pawel.pacana@gmail.com>
2012-08-18 23:11:37 +0200

    Fix broken test.

--- test/unit/result_test.rb
+++ test/unit/result_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
+require 'citrus/result'
 
 class ResultTest < UnitTestCase
   def test_should_not_be_valid_without_status
-    result = TestResult.new(:success)
-    result.status = nil
+    result = Citrus::Result.new(nil)
     refute result.valid?
   end
 end
