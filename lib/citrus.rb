5b4e141d177544adbbc1007fbe9c4b2e4c84acb0
Paweł Pacana <pawel.pacana@gmail.com>
2012-08-21 20:41:46 +0200

    Revert this bizzare style of requires and additional load path.
    Since code is namespaced under Citrus module let's have it all in one
    place.

--- lib/citrus.rb
+++ lib/citrus.rb
@@ -1,12 +1,12 @@
+require 'pathname'
+require 'securerandom'
+
+require 'bundler'
+Bundler.require(:default)
+
 module Citrus
   class << self
     attr_writer :cache_root, :build_root
-
-    def boot!
-      require_stdlib
-      require_bundler_default_group
-      adjust_load_path
-    end
 
     def build_root
       @build_root || root.join('builds')
@@ -26,27 +26,10 @@ module Citrus
 
     def db
       @db ||= begin
-                require 'citrus/persistence'
-                Citrus::Persistence.new
-              end
-    end
-
-    protected
-    def require_bundler_default_group
-      require 'bundler'
-      Bundler.require(:default)
-    end
-
-    def require_stdlib
-      require 'pathname'
-      require 'securerandom'
-    end
-
-    def adjust_load_path
-      $:.unshift Citrus.root.join('app').to_s
+        require 'citrus/persistence'
+        Citrus::Persistence.new
+      end
     end
   end
 end
-
-Citrus.boot!
 
